59389ba8ab76ca38317e6ed06190ea9d5bd51a5e
Sungmin Kim <chefminkim58@gmail.com>
2022-10-12 15:12:48 -0400

    update arduino motor control test

--- motor_control_test.py
+++ motor_control_test.py
@@ -1,31 +1,39 @@
 import serial
+import time
 
 print(serial.__version__)
 
-PORT = "COM4"
+PORT = "/dev/tty.usbmodem14201"
+
 SERIAL = 9600
 
 ser = serial.Serial(PORT, SERIAL)
 
 def test():
-    user_input = input("\n Command Motor forward / backward / quit : ")
+    user_input = input("\n Command Motor forward / backward / stop / quit : ")
 
-    if user_input == forward:
+    if user_input == "forward":
         print("Turning motor forwards")
         time.sleep(0.1)
-        ser.write(b'H')
+        ser.write(b'F')
         test()
 
-    elif user_input == backward:
+    elif user_input == "backward":
         print("Turning motor backwards")
         time.sleep(0.1)
-        ser.write(b'H')
+        ser.write(b'B')
+        test()
+    
+    elif user_input == "stop":
+        print("Stopping motor")
+        time.sleep(0.1)
+        ser.write(b'Q')
         test()
 
-    elif user_input == "q" or user input == "quit":
-        print("Exiting")
+    elif user_input == "q" or user_input == "quit":
+        print("Exiting Program")
         time.sleep(0.1)
-        ser.write(b'L')
+        ser.write(b'Q')
         ser.close()
     
     else:
