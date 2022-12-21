9b54520232cdeebf535864db843248b7ef62d76c
Sungmin Kim <chefminkim58@gmail.com>
2022-12-21 09:44:39 -0500

    added motor control python test script for two servos

---
+++ motor_control_two_servos.py
@@ -0,0 +1,57 @@
+import serial
+import time
+
+print(serial.__version__)
+
+PORT = "/dev/tty.usbmodem14201"
+
+SERIAL = 9600
+
+ser = serial.Serial(PORT, SERIAL)
+
+def test():
+    user_input = input("\n Command Motor left / right / forward / backward / quit : ")
+
+    if user_input == "left":
+        print("Turning left")
+        time.sleep(0.1)
+        ser.write(b'L')
+        test()
+
+    if user_input == "right":
+        print("Turning right")
+        time.sleep(0.1)
+        ser.write(b'R')
+        test()
+
+    if user_input == "forward":
+        print("Turning motor forwards")
+        time.sleep(0.1)
+        ser.write(b'U')
+        test()
+
+    elif user_input == "backward":
+        print("Turning motor backwards")
+        time.sleep(0.1)
+        ser.write(b'D')
+        test()
+    
+    elif user_input == "stop":
+        print("Stopping motor")
+        time.sleep(0.1)
+        ser.write(b'Q')
+        test()
+
+    elif user_input == "q" or user_input == "quit":
+        print("Exiting Program")
+        time.sleep(0.1)
+        ser.write(b'Q')
+        ser.close()
+    
+    else:
+        print("Invalid input")
+        test()
+
+time.sleep(2) # allow arduino connections to calibrate
+
+test()
